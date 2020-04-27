fc115dcebabef0ad894e66d78cf4cb46fbdaea97
TheAkshitS <akshit.s@outlook.com>
2020-04-27 05:53:07 +0530

    refactor goal module

--- store/goal.js
+++ store/goal.js
@@ -1,65 +1,20 @@
-import goalDb from '~/database/goalDb'
-
 export const state = () => ({ goals: [] })
 
 export const getters = { goals: (state) => state.goals }
 
 export const mutations = {
-  ADD_GOAL(state, goal) {
+  CREATE_GOAL(state, goal) {
     state.goals.push(goal)
   },
 
-  SET_GOALS(state, goals) {
-    state.goals = goals
+  UPDATE_GOAL(state, goal) {
+    const goalId = (element) => element.id === goal.id
+    const goalIndex = state.goals.findIndex(goalId)
+
+    state.goals[goalIndex].name = goal.name
   },
 
-  CLEAR_GOALS(state) {
-    state.goals = []
-  },
-
-  TOGGLE_GOAL(state, toggleGoal) {
-    const goal = state.goals.find((goal) => goal.id === toggleGoal.id)
-    if (goal) {
-      goal.completed = toggleGoal.completed
-    }
-  }
-}
-
-export const actions = {
-  async fetchGoals({ commit }) {
-    commit('CLEAR_GOALS')
-    const db = await goalDb.connect()
-    db.getAll().then(
-      (res) => {
-        commit('SET_GOALS', res)
-      },
-      (err) => {
-        throw err
-      }
-    )
-  },
-
-  async addGoal({ commit }, req) {
-    const db = await goalDb.connect()
-    db.add(req).then(
-      (res) => {
-        commit('ADD_GOAL', res)
-      },
-      (err) => {
-        throw err
-      }
-    )
-  },
-
-  async toggleGoal({ commit }, req) {
-    const db = await goalDb.connect()
-    db.put(req).then(
-      (res) => {
-        commit('TOGGLR_GOAL', res)
-      },
-      (err) => {
-        throw err
-      }
-    )
+  DELETE_GOAL(state, id) {
+    state.goals = state.goals.filter((goal) => goal.id !== id)
   }
 }
