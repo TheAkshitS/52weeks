c42dc78850a0594d2b5ad25061bcfdadd0bc400e
TheAkshitS <akshit.s@outlook.com>
2020-04-28 07:04:49 +0530

    update plugin to avoid ui module

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -2,6 +2,17 @@ import createPersistedState from 'vuex-persistedstate'
 
 export default ({ store }) => {
   window.onNuxtReady(() => {
-    createPersistedState({})(store)
+    createPersistedState({
+      reducer: (persistedState) => {
+        const stateFilter = Object.assign({}, persistedState)
+        const blackList = ['ui']
+        // For removing particular Vuex module from being persisted
+        blackList.forEach((item) => {
+          delete stateFilter[item]
+        })
+
+        return stateFilter
+      }
+    })(store)
   })
 }
