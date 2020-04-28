b2479e54d32253368dfae246466722f1075d87a4
TheAkshitS <akshit.s@outlook.com>
2020-04-28 07:04:02 +0530

    code refactor

---
+++ components/AppBottomNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-bottom-navigation
+    app
+    value="active.sync"
+    grow
+    color="primary"
+    class="d-sm-none"
+  >
+    <v-btn to="/">
+      <span>My Goals</span>
+      <v-icon>mdi-target</v-icon>
+    </v-btn>
+    <v-btn to="/about">
+      <span>About</span>
+      <v-icon>mdi-information-outline</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
