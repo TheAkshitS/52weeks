415781450cf758d24df417636595b0e3f7989a37
TheAkshitS <akshit.s@outlook.com>
2020-04-28 07:04:21 +0530

    add snackbar

---
+++ components/AppSnackbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-snackbar
+    :value="snackbar.visible"
+    top
+    color="primary"
+    :timeout="snackbar.timeout"
+  >
+    {{ snackbar.text }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  computed: {
+    snackbar() {
+      return this.$store.state.ui.snackbar
+    }
+  }
+}
+</script>
---
+++ store/ui.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+  snackbar: {}
+})
+
+export const mutations = {
+  SET_SNACKBAR(state, snackbar) {
+    state.snackbar = snackbar
+  }
+}
+
+export const actions = {
+  setSnackbar({ commit }, snackbar) {
+    snackbar.visible = true
+    snackbar.timeout = 1800
+
+    // TODO: Check visible
+    setTimeout(function() {
+      snackbar.visible = false
+      commit('SET_SNACKBAR', snackbar)
+    }, 1800)
+
+    commit('SET_SNACKBAR', snackbar)
+  }
+}
