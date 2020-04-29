62f2942a102247474b1090bf24b9baf1078c688b
TheAkshitS <akshit.s@outlook.com>
2020-04-29 06:43:39 +0530

    add settings page

---
+++ pages/settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>Settings</v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Settings'
+    }
+  }
+}
+</script>
