7fe4244c534f188cc828cf940af4173df0a99c96
TheAkshitS <akshit.s@outlook.com>
2020-04-30 21:24:55 +0530

    remove vibration

--- store/goal.js
+++ store/goal.js
@@ -5,7 +5,7 @@ export const getters = { goals: (state) => state.goals }
 export const mutations = {
   CREATE_GOAL(state, goal) {
     state.goals.push(goal)
-    window.navigator.vibrate(500)
+    // window.navigator.vibrate(500)
   },
 
   UPDATE_GOAL(state, goal) {
@@ -13,11 +13,11 @@ export const mutations = {
     const goalIndex = state.goals.findIndex(goalId)
 
     state.goals[goalIndex].name = goal.name
-    window.navigator.vibrate(500)
+    // window.navigator.vibrate(500)
   },
 
   DELETE_GOAL(state, id) {
     state.goals = state.goals.filter((goal) => goal.id !== id)
-    window.navigator.vibrate(500)
+    // window.navigator.vibrate(500)
   }
 }
