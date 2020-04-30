d8d98362eb209938efd6b3c3d091a1745968ac38
TheAkshitS <akshit.s@outlook.com>
2020-04-30 09:22:17 +0530

    update theme switcher

--- pages/settings.vue
+++ pages/settings.vue
@@ -7,12 +7,19 @@
         <v-icon left small>mdi-theme-light-dark</v-icon>Theme
       </h3>
 
-      <v-btn-toggle v-model="theme" mandatory rounded>
-        <v-btn>
-          <v-icon>mdi-brightness-7</v-icon>
-        </v-btn>
-        <v-btn>
-          <v-icon>mdi-brightness-4</v-icon>
+      <v-btn-toggle
+        v-model="selectedTheme"
+        dense
+        mandatory
+        rounded
+        @change="changeTheme"
+      >
+        <v-btn
+          v-for="theme in themeIcons"
+          :key="theme.name"
+          :value="theme.name"
+        >
+          <v-icon>{{ theme.icon }}</v-icon>
         </v-btn>
       </v-btn-toggle>
     </v-col>
@@ -22,9 +29,13 @@
         <v-icon left small>mdi-cash</v-icon>Currency
       </h3>
 
-      <v-btn-toggle v-model="currency" mandatory rounded>
-        <v-btn v-for="button in currencyIcons" :key="button.name">
-          <v-icon>{{ button.icon }}</v-icon>
+      <v-btn-toggle v-model="selectedCurrency" dense mandatory rounded>
+        <v-btn
+          v-for="currency in currencyIcons"
+          :key="currency.name"
+          :value="currency.name"
+        >
+          <v-icon>{{ currency.icon }}</v-icon>
         </v-btn>
       </v-btn-toggle>
     </v-col>
@@ -41,19 +52,31 @@ export default {
         { name: 'eur', icon: 'mdi-currency-eur' },
         { name: 'gbp', icon: 'mdi-currency-gbp' }
       ],
-      theme: 1,
-      currency: 0
+      themeIcons: [
+        { name: 'light', icon: 'mdi-brightness-7' },
+        { name: 'dark', icon: 'mdi-brightness-4' }
+      ],
+      selectedTheme: '',
+      selectedCurrency: 0
     }
   },
 
+  mounted() {
+    this.selectedTheme =
+      localStorage.getItem('darkMode') === 'true' ? 'dark' : 'light'
+  },
+
   methods: {
+    // TODO: Refactor using Vuex
     changeTheme() {
       const metaThemeColor = document.querySelector('meta[name=theme-color]')
 
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      if (this.selectedTheme === 'dark') this.$vuetify.theme.dark = true
+      else this.$vuetify.theme.dark = false
+
       if (localStorage)
-        localStorage.setItem('darkMode', this.$vuetify.theme.dark)
-      if (this.$vuetify.theme.dark) {
+        localStorage.setItem('darkMode', this.selectedTheme === 'dark')
+      if (this.selectedTheme) {
         metaThemeColor.setAttribute('content', '#212121')
       } else {
         metaThemeColor.setAttribute('content', '#0277bd')
