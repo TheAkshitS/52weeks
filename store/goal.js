24edd41294fa00076b74772adbf4ee5a1bf6baef
TheAkshitS <Akshit.S@outlook.com>
2020-05-01 12:35:10 +0530

    add getter

--- store/goal.js
+++ store/goal.js
@@ -1,11 +1,23 @@
 export const state = () => ({ goals: [] })
 
-export const getters = { goals: (state) => state.goals }
+export const getters = {
+  goals: (state) => state.goals,
+  goalAmount: (state) => (id) => {
+    const goal = state.goals.find((todo) => todo.id === id)
+    const totalWeeks = 52
+    let accountBalance = 0
+
+    for (let week = 1; week <= totalWeeks; week++) {
+      accountBalance = accountBalance + goal.amount * week
+    }
+
+    return accountBalance.toLocaleString()
+  }
+}
 
 export const mutations = {
   CREATE_GOAL(state, goal) {
     state.goals.push(goal)
-    // window.navigator.vibrate(500)
   },
 
   UPDATE_GOAL(state, goal) {
@@ -13,11 +25,9 @@ export const mutations = {
     const goalIndex = state.goals.findIndex(goalId)
 
     state.goals[goalIndex].name = goal.name
-    // window.navigator.vibrate(500)
   },
 
   DELETE_GOAL(state, id) {
     state.goals = state.goals.filter((goal) => goal.id !== id)
-    // window.navigator.vibrate(500)
   }
 }
