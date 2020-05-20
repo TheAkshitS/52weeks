1903435d4e65b92e847860bec7b53686549ec9c7
TheAkshitS <Akshit.S@Outlook.com>
2020-05-20 22:46:48 +0530

    add validation

--- plugins/currency.js
+++ plugins/currency.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 export default ({ store }) => {
   Vue.filter('currency', function(value) {
+    if (!value) return ''
     return store.state.settings.selectedCurrency.symbol + value
   })
 }
