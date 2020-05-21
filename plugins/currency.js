d9701c3cb0f92993acf7e0017d88f43ea46361a8
TheAkshitS <Akshit.S@Outlook.com>
2020-05-21 06:44:39 +0530

    fix not showing 0 amount

--- plugins/currency.js
+++ plugins/currency.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 export default ({ store }) => {
   Vue.filter('currency', function(value) {
-    if (!value) return ''
     return store.state.settings.selectedCurrency.symbol + value
   })
 }
