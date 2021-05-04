86cb915c9be488c45ed8ada97d18ce59b991464d
TheAkshitS <Akshit.S@outlook.com>
2021-05-05 00:00:13 +0530

    chore: disable vue/comment-directive

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: false,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'prettier',
-    'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-  }
+    "vue/comment-directive": 0
+  },
 }
